499d318bb3badbe48f8faf91f36d38c4ddf2c21e
Ssooob <sbean1713@gmail.com>
2023-01-12 22:32:23 +0900

    update

--- BaekJoon/Study/BJS_230112.py
+++ BaekJoon/Study/BJS_230112.py
@@ -16,14 +16,14 @@ time_100 = [0 for j in range(100)]
 for c in range(3):
     a,b = map(int,input().split())
     for cc in range(a,b) : 
-        time_100[cc] += 1
+        time_100[cc] += 1 
 #print(time_100)
 total_count = (time_100.count(1))*cost[0] + (time_100.count(2))*cost[1]*2 + (time_100.count(3))*cost[2]*3
 print(total_count)
 
 
 
-# 문자열 반복
+# 2675 자열 반복
 
 T = int(input())
 for t in range(1,T+1):
@@ -34,15 +34,17 @@ for t in range(1,T+1):
 
     print(new_string)
 
+
 # 1110 더하기 사이클
 
 num = int(input())
 N = num
 count = 0 
 while True :
+    # divmod 
     lnum = num//10 # 2
     rnum = num%10  # 6
-    lsnum = (rnum + lnum)%10 # 8 
+    lsnum = (rnum + lnum) %10 # 8 
     num = (rnum*10) + lsnum # 68
     count += 1
     if num == N:
@@ -58,17 +60,16 @@ strings = list(set(string))
 cnt_list = []
 
 # count 쓰는 경우
-for t in strings :
-    cnt = string.count(t)
-    cnt_list.append(cnt)
-#print(cnt_list)
+for t in strings : # string에 있는 애들 내에서 
+    cnt = string.count(t) # Mississipi
+    cnt_list.append(cnt) 
+print(cnt_list) # [1,1,4,4]
 
 if cnt_list.count(max(cnt_list)) >= 2:
     print("?")
 else :
     max_index=cnt_list.index(max(cnt_list))
     print(strings[max_index])
-
 
 
 # 1439 뒤집기
@@ -81,28 +82,10 @@ cnt = 0
 for i in range(int(len(numbers))-1): 
     if numbers[i] != numbers[i+1] :  
         cnt += 1  
-        
-        # 0에서 1로 바뀐 경우
-        #if numbers[i] == 0:
-        #    zero.append(i)
-        # 1에서 0으로 바뀐 경우
-        #elif numbers[i] == 1:
-        #    one.append(i)
-        
 print((cnt + 1) // 2)
-
-#total = zero + one # 4 4
-#total.sort() # [1, 3, 5, 7, 9, 11, 13, 18]
-
-# 11001100110011000001
-# 101010101 -> 1:5, 0:4 => 0을 뒤집는게 가장 나은 방법
-
-
-
 
 
 # 7567 그릇
-
 
 bowl = input() 
 height = 10
---
+++ BaekJoon/Study/Study.md
@@ -0,0 +1,4 @@
+## 문자
+* .ascii_uppercase : 모든 대문자 다 불러오기
+* import string as s
+    * s
